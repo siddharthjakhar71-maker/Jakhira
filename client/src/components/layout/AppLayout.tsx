fffe8f0ec1f9711cb8aa5901c7720b9bedfacef5
siddharthjakhar71-maker <siddharthjakhar71@gmail.com>
2026-03-22 22:57:48 +0530

    Make dashboard header scroll with content

--- client/src/components/layout/AppLayout.tsx
+++ client/src/components/layout/AppLayout.tsx
@@ -3,13 +3,15 @@ import { Header } from "./Header";
 
 export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="erp-app-frame">
+    <div className="erp-app-frame min-h-screen">
       <Sidebar />
       <div className="min-h-screen lg:pl-[280px]">
-        <Header />
-        <main className="px-4 pb-8 pt-6 md:px-6 xl:px-8">
-          <div className="mx-auto w-full max-w-[1600px]">{children}</div>
-        </main>
+        <div className="flex min-h-screen flex-col">
+          <Header />
+          <main className="flex-1 px-4 pb-8 pt-4 md:px-6 md:pt-5 xl:px-8 xl:pt-6">
+            <div className="mx-auto flex w-full max-w-[1600px] flex-1 flex-col">{children}</div>
+          </main>
+        </div>
       </div>
     </div>
   );
